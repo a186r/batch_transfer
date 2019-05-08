d054926bfdeb198409ad0ce3d3d709e381606f48
a186r <hi.aewalle@gmail.com>
2019-05-08 15:09:52 +0800

    创建一个ERC20 Token

---
+++ contracts/DogeCoinToken.sol
@@ -0,0 +1,18 @@
+pragma solidity ^ 0.5.0;
+
+import '/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
+import '/openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
+import '/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol';
+
+contract DogeCoinToken is ERC20, ERC20Detailed, ERC20Mintable {
+    constructor(
+
+    )
+        public
+        ERC20Mintable()
+        ERC20Detailed("dogecoin", "DGCT", 18)
+        ERC20()
+    {
+        mint(msg.sender,2000);
+    }
+}
---
+++ test/Test.js
@@ -0,0 +1,1 @@
+var BatchTransfer = artifacts.require("BatchTransfer")
